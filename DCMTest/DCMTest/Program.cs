bf399ab697483213d4b78c29f50319a081a80659
Sebastian <sebastian.jazmin.nw@gmail.com>
2023-08-09 23:20:23 -0400

    Added Read Note Function

--- DCMTest/DCMTest/Program.cs
+++ DCMTest/DCMTest/Program.cs
@@ -2,30 +2,81 @@
 //Task: Create a console=based note-taking application using .NET Core
 //Time Start: 8:05 PM
 
+using System.Text;
+
 Console.Write("Note Taking Application:\n\n"); //Intro Message
 
 
 String notes_path = "..\\..\\..\\notes"; // Path for notes folder
 
-bool go = true; //controls main menu loop
+bool go = true; //Controls main menu loop
 //Menu Loop
 while (go)
 {
     //Writing main menu to console
-    Console.Write("1. Create a Note\n2. View All Notes\n3. Delete Note\n4. Exit\n\nInput Selection (1-4):  ");
+    Console.Write("\n1. Create a Note\n2. View All Notes\n3. Delete Note\n4. Exit\n\nInput Selection (1-4):  ");
     String? user_input = Console.ReadLine();
     switch (user_input)
     {
         case "1": //User wants to create a note
-
+            //If notes folder does not exist, create it
+            if (!Directory.Exists(notes_path))
+            {
+                Directory.CreateDirectory(notes_path);
+            }
             String note_name = GetValidNoteName();
 
             Console.WriteLine("Input contents for " + note_name + " note:"); // Prompt for contents
             String? note_text = Console.ReadLine();// Read user input
             File.WriteAllText("..\\..\\..\\notes\\" + note_name + ".txt", note_name + ":\n" + note_text); //Create note file
-            break;
+            break; //Return to main menu
 
-        case "2":
+        case "2": //User wants to read a note
+            //If notes folder does not exist, create it
+            if (!Directory.Exists(notes_path))
+            {
+                Directory.CreateDirectory(notes_path);
+            }
+
+            Console.WriteLine("All Saved Notes:");// Intro
+
+            List<String?>? existing_names = Directory.GetFiles(notes_path).Select(Path.GetFileName).ToList();//Get all note names
+            
+            //Write all note names
+            for (int i = 0; i < existing_names.Count(); i++)
+            {
+                Console.WriteLine(i + 1 + ". " + existing_names[i]);
+            }
+
+            //loop for selecting which note to read
+            while (true)
+            {
+                int user_selection = UserInputToInt();
+                
+                if (user_selection == -1)//-1 returns to menu
+                {
+                    break;
+                }
+
+                user_selection = user_selection - 1;
+                //Check if selection is in range
+                if (user_selection < 0 || user_selection > existing_names.Count() - 1)
+                {
+                    Console.WriteLine("Please select a number in range or -1 to exit");
+                }
+                else
+                {
+                    //Print the note
+                    string[] lines = File.ReadAllLines("..\\..\\..\\notes\\" + existing_names[user_selection], Encoding.UTF8);
+                    Console.WriteLine(lines[0]); //Print name
+                    //Loop to print rest of file
+                    for (int i = 0; i++ < lines.Length; i++)
+                    {
+                        Console.WriteLine(lines[i]);
+                    }
+                }
+                break; //Return to main menu
+            }
 
             break;
         case "3":
@@ -42,6 +93,23 @@ while (go)
 
 Console.Write("Thank you for using this app. Have a wonderful day!");
 
+int UserInputToInt()
+{
+    //Turn User Input to an int
+    while (true)
+    {
+        Console.Write("Please input selection (or -1 to exit): ");
+        if (int.TryParse(Console.ReadLine(), out int selection))
+        {
+            return selection;
+        }
+        else
+        {
+            Console.WriteLine("Please input an integer");
+        }
+
+    }
+}
 
 String GetValidNoteName()
 {
@@ -55,12 +123,6 @@ String GetValidNoteName()
         if (!String.IsNullOrEmpty(note_name)) //Check for null
         {
             String filename = note_name + ".txt";// add .txt to compare to existing notes
-
-            //If notes folder does not exist, create it
-            if (!Directory.Exists(notes_path))
-            {
-                Directory.CreateDirectory(notes_path);
-            }
 
             var existing_names = Directory.GetFiles(notes_path).Select(Path.GetFileName); //Grab all note names and put them into the var
 
