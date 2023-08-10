17ebb49e902f47d16a78bda6e2b7978883441bf6
Sebastian <sebastian.jazmin.nw@gmail.com>
2023-08-09 20:06:23 -0400

    Created project

---
+++ DCMTest/DCMTest/Program.cs
@@ -0,0 +1,16 @@
+﻿//Sebastian Jazmin DIgital CM Software Engineer Coding Test
+//Task: Create a console=based note-taking application using .NET Core
+//Time Start: 8:00 PM
+
+
+//Menu Loop
+while (true)
+{
+    //Writing main menu to console
+    Console.Write("Note Taking Application:\n\n1. Create a Note\n2. View All Notes\n3. Delete Note\n4. Exit\n\nPlease input selection: ");
+    String selection = Console.ReadLine();
+    Console.Clear();
+}
+
+/*String note_name = "test";
+File.WriteAllText("..\\..\\..\\notes\\" + note_name + ".txt", note_name + ":\n" +"LOL");*/
